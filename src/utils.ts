78800f0ee875e8e8a1d0921349a3066b76274295
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-12 00:06:51 +0400

    logger

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,9 @@
 export const logJob = (cb: Function) => (job: string) => async () => {
 	try {
-		console.group(`Job: ${job}`);
-		console.log(`Starting job: ${job}`);
+		console.log(`Running job: ${job}`);
 		await cb();
-		console.log(`Job ${job} completed successfully`);
+		console.log(`Job ${job} completed`);
 	} catch (e) {
-		console.error(`Job ${job} failed with error:`, e);
-	} finally {
-		console.groupEnd();
+		console.log(`Job ${job} failed with error: ${e}`);
 	}
 };
